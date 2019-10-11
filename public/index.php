98c8ea3b392e7fe5782dc6c949184660bd76cb6e
Ruman Saleem <ruman63@gmail.com>
2019-10-11 18:16:45 +0530

    refactor: clean up if-else routing

--- public/index.php
+++ public/index.php
@@ -5,32 +5,31 @@ require_once __DIR__ . '/../app/bootstrap.php';
 require_once __DIR__ . '/../app/database.php';
 require_once __DIR__ . '/../app/helpers.php';
 
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-$path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+$routes = [
+    'GET' => [
+        '/' => 'HomeController',
+        '/about' => 'AboutController',
+        '/contact' => 'ContactController',
+        '/login' => 'LoginFormController',
+        '/signup' => 'SignupFormController',
+    ],
+    'POST' => [
+        '/login' => 'LoginController',
+        '/signup' => 'SignupController',
+        '/logout' => 'LogoutController',
+        '/posts' => 'CreatePostController',
+    ]
+];
 
-if ($requestMethod == 'GET') {
-    if ($path == '') {
-        require __DIR__ . '/../app/Controllers/HomeController.php';
-    } else if ($path == 'about') {
-        require __DIR__ . '/../app/Controllers/AboutController.php';
-    } else if ($path == 'contact') {
-        require __DIR__ . '/../app/Controllers/ContactController.php';
-    } else if ($path == 'login') {
-        require __DIR__ . '/../app/Controllers/LoginFormController.php';
-    } else if ($path == 'signup') {
-        require __DIR__ . '/../app/Controllers/SignupFormController.php';
-    } 
-} else if ($requestMethod == 'POST') {
-    if ($path == 'login') {
-        require __DIR__ . '/../app/Controllers/LoginController.php';
-    } else if ($path == 'signup') {
-        require __DIR__ . '/../app/Controllers/SignupController.php';
-    } else if ($path == 'logout') {
-        require __DIR__ . '/../app/Controllers/LogoutController.php';
-    } else if($path == 'posts') {
-        require __DIR__ . '/../app/Controllers/CreatePostController.php';
-    }
-} else {
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$path = '/' . trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+
+if (!array_key_exists($path, $routes[$requestMethod])) {
     http_response_code(404);
-    echo '404 Not Found!';
+    echo '<h1>404 - Not Found!</h1>';
+    return;
 }
+
+$controller = $routes[$requestMethod][$path];
+
+require __DIR__ . "/../app/Controllers/{$controller}.php";
