23d0c580f81b7993782b3558e49ac37adc8fe879
Ruman Saleem <ruman63@gmail.com>
2019-10-12 12:39:03 +0530

    refactor: move core classes to core directory

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 
 require_once __DIR__ . '/../app/bootstrap.php';
 
-require_once __DIR__ . '/../app/Request.php';
-require_once __DIR__ . '/../app/Router.php';
-require_once __DIR__ . '/../app/Database.php';
+require_once __DIR__ . '/../app/Core/Request.php';
+require_once __DIR__ . '/../app/Core/Router.php';
+require_once __DIR__ . '/../app/Core/Database.php';
 require_once __DIR__ . '/../app/helpers.php';
 
 require_once __DIR__ . '/../app/Controllers/Controller.php';
