7ec9360a54bdfbe9e613cf0820707312a1669a3a
Ruman Saleem <ruman63@gmail.com>
2019-10-12 22:08:17 +0530

    autoload: use classmap autoloader

--- public/index.php
+++ public/index.php
@@ -2,20 +2,7 @@
 
 require_once __DIR__ . '/../app/bootstrap.php';
 
-require_once __DIR__ . '/../app/Core/Request.php';
-require_once __DIR__ . '/../app/Core/Router.php';
-require_once __DIR__ . '/../app/Core/Database.php';
-require_once __DIR__ . '/../app/Core/QueryBuilder.php';
-require_once __DIR__ . '/../app/Core/Auth.php';
-
-require_once __DIR__ . '/../app/helpers.php';
-
-require_once __DIR__ . '/../app/Controllers/Controller.php';
-require_once __DIR__ . '/../app/Controllers/PagesController.php';
-require_once __DIR__ . '/../app/Controllers/LoginController.php';
-require_once __DIR__ . '/../app/Controllers/LogoutController.php';
-require_once __DIR__ . '/../app/Controllers/SignupController.php';
-require_once __DIR__ . '/../app/Controllers/PostController.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $router = Router::getInstance();
 $request = Request::createFromGlobals();
